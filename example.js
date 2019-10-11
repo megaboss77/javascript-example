31368c3ec4841fb6e1ecc36eb6a3e80e7ceccd75
Nattapat Sutapakorn <nattsut@th.ibm.com>
2019-10-11 17:37:27 +0700

    format

--- example.js
+++ example.js
@@ -1,11 +1,15 @@
 //javascript basics tutorial 
-let numbers = [1,2,3];
+let numbers = [1, 2, 3];
 
 let employees = [
-    {'name':'boss',
-    'age':20},
-    {'name':'moo',
-    'age':19}
+    {
+        'name': 'boss',
+        'age': 20
+    },
+    {
+        'name': 'moo',
+        'age': 19
+    }
 
 ];
 
@@ -14,33 +18,33 @@ const x = 1;
 
 //if else short hand
 //if x>10 then color = red else color = blue 
-const color = x > 10 ? 'red':'blue';
+const color = x > 10 ? 'red' : 'blue';
 console.log(color);
 
 // == mean check only value 
 // === mean also check the data type 
-if(x===1){
+if (x === 1) {
     console.log(1);
 }
 
 //this is for each
-for(let i of numbers){
+for (let i of numbers) {
     console.log(i);
 }
 //using foreach 
-numbers.forEach(function(number){
-console.log(number);
+numbers.forEach(function (number) {
+    console.log(number);
 });
 //arrow function
-numbers.forEach((number)=>{
+numbers.forEach((number) => {
     console.log(number);
 });
 
 
 //foreach, map , filter !!!
-let filtered = employees.filter(function(employee){
-    return employee.age>19;
-}).map(function(employee){
+let filtered = employees.filter(function (employee) {
+    return employee.age > 19;
+}).map(function (employee) {
     return employee.name;
 });
 
@@ -48,29 +52,29 @@ console.log(filtered);
 
 //function 
 //can assign the initial vaue to a function
-function sum( num1 = 1,num2 = 2){
+function sum(num1 = 1, num2 = 2) {
     return num1 + num2;
 }
 //is same with 
 
 //no need to have curlly brances and return keyword when it is one sentence 
-const sum2 = ( num1 = 1,num2 = 2) => num1 + num2;
+const sum2 = (num1 = 1, num2 = 2) => num1 + num2;
 
 console.log(sum());
 
 
 //create the class
-class Student{
-    constructor(firstName, lastName){
+class Student {
+    constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    getName(){
+    getName() {
         return `${this.firstName} ${this.lastName}`;
     }
 }
 
 //instantiate the object 
-const boss =new Student("Nattapat","Sutapakorn");
+const boss = new Student("Nattapat", "Sutapakorn");
 
 console.log(boss.getName());
